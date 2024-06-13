437dbcbc10fda5029b45c0e80d85c25eceac1fa3
shisyamotonono <c0b221579f@edu.teu.ac.jp>
2024-06-13 16:38:33 +0900

    こうかとんの描写

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
