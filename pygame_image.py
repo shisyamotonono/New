2ada32a162f5482b25e27fdee91e956337fe9066
shisyamotonono <c0b221579f@edu.teu.ac.jp>
2024-06-24 13:39:40 +0900

    背景画像のワープ対策

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -17,9 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-         x = tmr%800
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0]) 
         kk_rct = kk_img.rect()
         kk_rct.center = 300,200
         screen.bilt(kk_img,kk_rct)
