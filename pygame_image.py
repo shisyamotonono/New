24cce2764c91e363c947d55fb8a9965caea938ac
shisyamotonono <c0b221579f@edu.teu.ac.jp>
2024-06-24 13:42:38 +0900

    押下キーに応じたこうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  # こうかとんRectの抽出
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -23,8 +25,16 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0]) 
-        kk_rct = kk_img.rect()
-        kk_rct.center = 300,200
+
+        key_lst = pg.key.get_pressed()  # 全キーの押下状態を取得
+        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
+            kk_rct.move_ip(+1, 0)
         screen.bilt(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
